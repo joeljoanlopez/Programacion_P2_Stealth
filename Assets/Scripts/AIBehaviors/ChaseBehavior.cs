295b1a257d77ba6be22b949f63109335f616d2bc
Joloharo <joeljoanlopez@gmail.com>
2024-02-13 16:00:30 +0100

    Chase y Idle hechos, falta patrol con los nodos

--- Assets/Scripts/AIBehaviors/ChaseBehavior.cs
+++ Assets/Scripts/AIBehaviors/ChaseBehavior.cs
@@ -1,25 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ChaseBehavior : StateMachineBehaviour
 {
+    [SerializeField] private float _speed = 0.0f;
+    [SerializeField] private float _visionRange = 0.0f;
+    [SerializeField] private Transform _target;
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool("_isChasing", IsPlayerClose(animator.transform));
+
+        Vector2 _dir = _target.position - animator.transform.position;
+        animator.transform.position += (Vector3)_dir.normalized * _speed * Time.deltaTime;
+    }
+
+    private bool IsPlayerClose(Transform transform)
+    {
+        var dist = Vector3.Distance(transform.position, _target.position);
+        return (dist < _visionRange);
+    }
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     //{
-    //    
-    //}
-
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
     //}
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     //{
-    //    
+    //
     //}
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
